ff22953baa10463d5368b21f9fd7fb2e13b39316
benita-laakso <benitalaaksofi@yahoo.com>
2019-09-16 19:06:31 +0300

    resolved

--- src/TodoList.js
+++ src/TodoList.js
@@ -7,27 +7,25 @@ import Checkbox from '@material-ui/core/Checkbox';
 import IconButton from '@material-ui/core/IconButton';
 import DeleteIcon from '@material-ui/icons/Delete';
 
-const TodoList =({todos,deleteTodo})=>(
-<List>
-  //map loops over the toods array in index js
-  //becouse of the index the computer knows what to delete
-{todos.map((todo,index)=>(
-  <ListItem key={index.toString()} dense button>
-  <Checkbox tableIndex={-1} disableRipple/>
-  <ListItemText primary ={todo}/>
-  <ListItemSecondaryAction>
-  <IconButton
-  aria-label="Delete"
-  onClick={()=>{
-    deleteTodo(index);
-  }}
-  >
-  <DeleteIcon/>
-  </IconButton>
-  </ListItemSecondaryAction>
-  </ListItem>
-))}
-</List>
+const TodoList = ({ todos, deleteTodo }) => (
+  <List>
+    {todos.map((todo, index) => (
+      <ListItem key={index.toString()} dense button>
+        <Checkbox tabIndex={-1} disableRipple />
+        <ListItemText primary={todo} />
+        <ListItemSecondaryAction>
+          <IconButton
+            aria-label="Delete"
+            onClick={() => {
+              deleteTodo(index);
+            }}
+          >
+            <DeleteIcon />
+          </IconButton>
+        </ListItemSecondaryAction>
+      </ListItem>
+    ))}
+  </List>
 );
 
 export default TodoList;
--- src/index.js
+++ src/index.js
@@ -1,16 +1,34 @@
-import React, { useState } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
+import Typography from '@material-ui/core/Typography';
 import TodoForm from './TodoForm';
 import TodoList from './TodoList';
-import Typography from '@material-ui/core/Typography';
+import useTodoState from './useTodoState';
 import './styles.css';
 
 const App = () => {
-  const [todos, setTodos] = useState([]); //Space list
+  const { todos, addTodo, deleteTodo } = useTodoState([]);
+
   return (
-    <div className="App"/>
+    <div className="App">
+      <Typography component="h1" variant="h2">
+        Todos
+      </Typography>
+
+      <TodoForm
+        saveTodo={(todoText) => {
+          const trimmedText = todoText.trim();
+
+          if (trimmedText.length > 0) {
+            addTodo(trimmedText);
+          }
+        }}
+      />
+
+      <TodoList todos={todos} deleteTodo={deleteTodo} />
+    </div>
   );
 };
-{}
+
 const rootElement = document.getElementById('root');
 ReactDOM.render(<App />, rootElement);
